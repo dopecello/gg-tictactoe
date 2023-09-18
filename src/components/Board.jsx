cce9d69ff753c25a95aa84fd430a535c93926826
Kristina Schaberg <kristinaschaberg@Kristinas-MacBook-Pro.local>
2023-09-18 10:50:18 -0400

    board changes color on winning row

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -6,7 +6,7 @@ import PlayerRegistration from './PlayerRegistration';
 const Board = () => {
     const [squares, setSquares] = useState(Array(9).fill(null));
     const [isXNext, setIsXNext] = useState(true);
-    const [winner, setWinner] = useState(null);
+    const [winner, setWinner] = useState({ player: null, line: [] });
     const [players, setPlayers] = useState({
         X: null,
         O: null
@@ -35,26 +35,29 @@ const Board = () => {
         for (let i = 0; i < lines.length; i++) {
             const [a, b, c] = lines[i];
             if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-                return squares[a];
+                return { winner: squares[a], winningLine: [a, b, c] };
             }
         }
         return null;
     };
 
     const checkTie = () => {
-        return squares.every(square => square !== null) && winner === null;
+        return squares.every(square => square !== null) && winner.player === null;
     };
 
     const handleSquareClick = index => {
-        if (!gameStarted || squares[index] || winner) return;
+        if (!gameStarted || squares[index] || winner.player) return;
 
         const newSquares = squares.slice();
         newSquares[index] = isXNext ? 'X' : 'O';
         setSquares(newSquares);
 
-        const winningPlayer = checkWinner(newSquares);
-        if (winningPlayer) {
-            setWinner(players[winningPlayer]);
+        const winningData = checkWinner(newSquares);
+        if (winningData) {
+            setWinner({
+                player: players[winningData.winner],
+                line: winningData.winningLine
+            });
             return;
         }
 
@@ -66,15 +69,15 @@ const Board = () => {
     const isCurrentPlayerDisplayVisible = gameStarted && !winner && isTie === false;
 
     const squareStyles = [
-        "border-slate-500/40 border-b-4 border-r-4",
-        "border-slate-500/40 border-t-4 border-r-4 border-b-4",
-        "border-slate-500/40 border-t-4 border-r-4",
-        "border-slate-500/40 border-l-4 border-b-4 border-r-4",
-        "border-slate-500/40 border-4",
-        "border-slate-500/40 border-l-4 border-t-4 border-r-4",
-        "border-slate-500/40 border-l-4 border-b-4",
-        "border-slate-500/40 border-t-4 border-l-4 border-b-4",
-        "border-slate-500/40 border-t-4 border-l-4"
+        "border-slate-700 border-b-4 border-r-4",
+        "border-slate-700 border-t-4 border-r-4 border-b-4",
+        "border-slate-700 border-t-4 border-r-4",
+        "border-slate-700 border-l-4 border-b-4 border-r-4",
+        "border-slate-700 border-4",
+        "border-slate-700 border-l-4 border-t-4 border-r-4",
+        "border-slate-700 border-l-4 border-b-4",
+        "border-slate-700 border-t-4 border-l-4 border-b-4",
+        "border-slate-700 border-t-4 border-l-4"
     ];
 
     return (
@@ -91,17 +94,18 @@ const Board = () => {
                         {squares.map((square, index) => (
                             <div
                                 key={index}
-                                className={`square w-24 h-24 flex justify-center items-center bg-purple-300 transition-opacity duration-500 ${squareStyles[index]}`}
+                                className={`square w-24 h-24 flex justify-center items-center bg-purple-300 transition-opacity duration-500 ${squareStyles[index]}
+                                    ${winner.line.includes(index) ? 'bg-purple-800' : ''}`}
                                 onClick={() => handleSquareClick(index)}
                             >
-                                {square === 'X' && <AiOutlineClose size={38} className='text-slate-700' />}
-                                {square === 'O' && <IoMdRadioButtonOff size={36} className='text-slate-700' />}
+                                {square === 'X' && <AiOutlineClose size={38} className={`text-slate-700 ${winner.line.includes(index) ? 'text-white' : ''}`} />}
+                                {square === 'O' && <IoMdRadioButtonOff size={36} className={`text-slate-700 ${winner.line.includes(index) ? 'text-white' : ''}`} />}
                             </div>
                         ))}
                     </div>
 
                     <div className="text-2xl font-bold h-10">
-                        {winner ? `${winner} wins!` : isTie ? 'It\'s a tie!' : ''}
+                        {winner.player ? `${winner.player} wins!` : isTie ? 'It\'s a tie!' : ''}
                     </div>
                 </>
             )}
