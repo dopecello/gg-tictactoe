612a974bbf9ac46ed67c8b17759e6a55132ff662
Kristina Schaberg <kristinaschaberg@Kristinas-MacBook-Pro.local>
2023-09-19 11:58:15 -0400

    set rules for player registration

--- src/components/PlayerRegistration.jsx
+++ src/components/PlayerRegistration.jsx
@@ -5,6 +5,7 @@ const PlayerRegistration = ({ onPlayersRegistered }) => {
     const [playerX, setPlayerX] = useState("");
     const [playerO, setPlayerO] = useState("");
     const [isPlayerXRegistered, setPlayerXRegistered] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
 
     const handlePlayerX = () => {
         if (playerX.trim()) {
@@ -14,9 +15,15 @@ const PlayerRegistration = ({ onPlayersRegistered }) => {
 
     const handleStartGame = () => {
         if (playerO.trim()) {
+            if (playerO.toLowerCase() === playerX.toLowerCase()) {
+                setErrorMessage("Players must have different names.");
+                return;
+            }
+
+            setErrorMessage("");
             onPlayersRegistered(playerX, playerO);
         }
-    }
+    };
 
     const isPlayerXValid = playerX.length > 0 && playerX.length <= 30;
 
@@ -28,9 +35,10 @@ const PlayerRegistration = ({ onPlayersRegistered }) => {
                     type='text'
                     value={playerX}
                     onChange={e => setPlayerX(e.target.value)}
-                    maxLength={30}
+                    maxLength={15}
                     className='text-center border-b-2 border-black bg-purple-300 focus:outline-none w-[20rem]'
                 />
+                <div className="text-red-500 text-xs text-center h-2">{errorMessage}</div>
                 <div
                     className={`flex justify-center mx-auto mt-10 text-center p-3 border-2 border-black w-24 
                                 ${isPlayerXValid ? 'hover:bg-purple-800 hover:text-white transition-colors cursor-pointer' : 'cursor-not-allowed'}`}
@@ -45,10 +53,11 @@ const PlayerRegistration = ({ onPlayersRegistered }) => {
                     type='text'
                     value={playerO}
                     onChange={e => setPlayerO(e.target.value)}
-                    maxLength={30}
+                    maxLength={15}
                     className='text-center border-b-2 border-black bg-purple-300 focus:outline-none w-[20rem]'
 
                 />
+                <div className="text-red-500 text-xs text-center h-2">{errorMessage}</div>
                 <div
                     className={`flex justify-center mx-auto mt-10 text-center p-3 border-2 border-black w-24 
                                 ${isPlayerXValid ? 'hover:bg-purple-800 hover:text-white transition-colors cursor-pointer' : 'cursor-not-allowed'}`}
