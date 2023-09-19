ac36dc823ce5b25438e6d88df5a588486ecc3033
dopecello <williamlocke.dev@gmail.com>
2023-09-19 14:33:57 -0400

    add help-menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { BsInfoCircle } from 'react-icons/bs'
 import { LuTrophy } from 'react-icons/lu'
 import Leaderboard from './Leaderboard';
+import HelpMenu from './HelpMenu';
 
 const Header = () => {
 
@@ -11,14 +12,25 @@ const Header = () => {
         setLeaderboardActive(!isLeaderboardActive);
     }
 
+    const [isHelpMenuActive, setHelpMenuActive] = useState(false);
+
+    const handleHelpMenu = () => {
+        setHelpMenuActive(!isHelpMenuActive);
+    }
+
 
     return (
         <>
+            <HelpMenu
+                isActive={isHelpMenuActive}
+                toggleHelpMenu={handleHelpMenu}
+            />
             <Leaderboard
                 isActive={isLeaderboardActive}
-                toggleLeaderboard={handleLeaderboard}/>
+                toggleLeaderboard={handleLeaderboard}
+            />
             <div className='fixed top-0 left-0 w-full flex flex-row justify-between bg-purple-300 text-center p-4 z-50'>
-                <BsInfoCircle size={33} className='cursor-pointer hover:text-white' />
+                <BsInfoCircle size={33} className='cursor-pointer hover:text-white' onClick={handleHelpMenu} />
                 <LuTrophy size={32} className='cursor-pointer hover:text-white' onClick={handleLeaderboard} />
             </div>
         </>
